5853d3dddd5fab23d7d9ef9f4701f16a9161b342
Kerk en IT <info@kerkenit.nl>
2016-12-07 22:41:52 +0100

    Revert to 021d2fb1a2d8144f4f84d5ea86b3e6a87e7e8300 and fixed adding the correct way

--- README.md
+++ README.md
@@ -10,18 +10,10 @@ In the flow manager you can select the server, and send the command.
 
 # Usefull?
 
-* Trigger a backup script of your server from Homey
-* Useful to start a playlist from Spotify https://github.com/dronir/SpotifyControl on your Mac.
+* Create a backup of your server through Homey
+* Useful to start a playlist from [Spotify](https://github.com/dronir/SpotifyControl) on your Mac.
 
 #Remarks
 
 * This won't work with certificates. Only username and password is allowed.
 * You can't create an connection to the Homey, because SSH access to the Homey is secured by Athom
-
-#Changelog
-
-##v0.1.0
-
-* Rewrite of adding devices. Oldder added devices are unsupported.
-* Check on adding if server exist and respond to prevent app hangs and a reboot of Homey
-* Removed unnecessary files to save storage on your Homey
--- drivers/ssh/driver.js
+++ drivers/ssh/driver.js
@@ -1,104 +1,86 @@
-/* global Homey, module __ */
-(function() {
-	'use strict';
-}());
-// a list of devices, with their 'id' as key
-// it is generally advisable to keep a list of
-// paired and active devices in your driver's memory.
-var devices = {};
 var tempServerName = '',
 	tempHostName = '',
 	tempUsername = '',
 	tempPassword = '',
 	tempPort = 22;
-// the `init` method is called when your driver is loaded for the first time
-module.exports.init = function(devices_data, callback) {
-	devices_data.forEach(function(device_data) {
-		initDevice(device_data);
-	});
-	callback();
-};
-// the `added` method is called is when pairing is done and a device has been added
-module.exports.added = function(device_data, callback) {
-	initDevice(device_data);
-	callback(null, true);
-};
-// the `delete` method is called when a device has been deleted by a user
-module.exports.deleted = function(device_data, callback) {
-	delete devices[device_data.id];
-	callback(null, true);
-};
-// the `pair` method is called when a user start pairing
 module.exports.pair = function(socket) {
-	socket.on('get_devices', function(data, callback) {});
+	// socket is a direct channel to the front-end
+	// this method is run when Homey.emit('list_devices') is run on the front-end
+	// which happens when you use the template `list_devices`
 	socket.on('list_devices', function(data, callback) {
-		var device_data = {
-			name: data.name,
+		console.log("SSH Client - list_devices tempHostName is", tempHostName);
+		var devices = [{
 			data: {
-				id: data.data.id,
-				hostname: data.settings.hostname,
-				username: data.settings.username,
-				password: data.settings.password,
-				port: data.settings.port
+				id: tempHostName,
+				hostname: tempHostName,
+				username: tempUsername,
+				password: tempPassword,
+				port: parseInt(tempPort, 10)
 			},
-			capabilities: []
-		};
+			name: tempServerName
+		}];
+		callback(null, devices);
+	});
+	// this is called when the user presses save settings button in start.html
+	socket.on('get_devices', function(data, callback) {
+		// Set passed pair settings in variables
+		tempHostName = data.hostname;
+		tempUsername = data.username;
+		tempPassword = data.password;
+		tempPort = data.port;
 
-		callback(null, [device_data]);
+		var devices = [{
+			data: {
+				id: tempHostName,
+				hostname: tempHostName,
+				username: tempUsername,
+				password: tempPassword,
+				port: parseInt(tempPort, 10)
+			},
+			name: tempServerName
+		}];
 
+		if (data.serverName === undefined || data.serverName === null || data.serverName.length === 0) {
+			tempServerName = tempHostName;
+		} else {
+			tempServerName = data.serverName;
+		}
+		console.log("SSH Client - got get_devices from front-end, tempHostName =", tempHostName);
+		callback(null, devices);
 	});
 };
 // flow action handlers
 Homey.manager('flow').on('action.command', function(callback, args) {
-	Homey.log("SSH Client - sending " + args.command + "\n to " + args.device.id);
+	console.log("SSH Client - sending " + args.command + "\n to " + rgs.device.id);
 	module.exports.getSettings(args.device, function(err, settings) {
+		Homey.log(settings);
 		var Client = require('ssh2').Client;
 		var conn = new Client();
-		var Host = {
+		conn.on('ready', function() {
+			conn.exec(args.command, function(err, stream) {
+				if (err) {
+					console.log(err);
+					callback(null, false);
+				};
+				stream.on('close', function(code, signal) {
+					conn.end();
+					callback(null, true);
+				}).on('data', function(data) {
+					console.log('STDOUT: ' + data);
+					callback(null, true);// we've fired successfully
+				}).stderr.on('data', function(data) {
+					console.log('STDERR: ' + data);
+					callback(null, false);
+				});
+			});
+		}).on('keyboard-interactive', function(name, instr, lang, prompts, cb) {
+			cb([settings.password]);
+		}).connect({
 			host: settings.hostname,
 			port: settings.port,
 			tryKeyboard: true,
 			username: settings.username,
 			password: settings.password
-		};
-		conn.on('ready', function() {
-			conn.exec(args.command, function(err, stream) {
-				if (err) {
-					Homey.log(err);
-					callback(null, false);
-				}
-				stream.on('close', function(code, signal) {
-					conn.end();
-					callback(null, true);
-				}).on('data', function(data) {
-					Homey.log('STDOUT: ' + data);
-					callback(null, true); // we've fired successfully
-				}).stderr.on('data', function(data) {
-					Homey.log('STDERR: ' + data);
-					callback(null, false);
-				});
-			});
-		}).on('keyboard-interactive', function(name, instr, lang, prompts, cb) {
-			cb([Host.password]);
-		}).on('error', function(err) {
-			Homey.log(err);
-		}).connect(Host);
+		});
 	});
 });
-// a helper method to get a device from the devices list by it's device_data object
-var getDeviceByData = function(device_data) {
-		var device = devices[device_data.id];
-		if (typeof device === 'undefined') {
-			return new Error("invalid_device");
-		} else {
-			return device;
-		}
-	};
-// a helper method to add a device to the devices list
-var initDevice = function(device_data) {
-		devices[device_data.id] = {};
-		devices[device_data.id].data = device_data;
-		module.exports.getSettings(device_data, function(err, settings) {
-			devices[device_data.id].settings = settings;
-		});
-	};
